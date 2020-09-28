e0b7249f3eda1095ecaead8ca51cce7e181db729
dashahello <st.daryaa@gmail.com>
2020-09-28 19:24:24 +0300

    + basic login/logout, user sessions

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/sc-demo', {
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const MONGO_URL = 'mongodb://localhost:27017/sc-demo';
+
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+const sessionStore = new MongoDBStore({
+  uri: MONGO_URL,
+  collection: 'sessions',
+  connectionOptions: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+});
+
+sessionStore.on('error', function (error) {
+  console.log('sessionStore error', error);
+});
+
 const User = mongoose.model('User', { username: String, password: String });
 
-// const kitty = new Cat({ name: '' });
+// const kitty = new Cat({ name: 'Zildjian' });
 // kitty.save().then(() => console.log('meow'));
-
-// (async () => {
-//   const cats = await Cat.findOne({});
-//   console.log(cats);
-// })();
 
 const app = express();
 
 const PORT = 3005;
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: true }));
+
+app.use(
+  session({
+    secret: '9y32u4n324n234ui2423n4;l73;4o6p',
+    cookie: {
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+    },
+    store: sessionStore,
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
@@ -30,21 +56,52 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/login', (req, res) => {
-  console.log('login', req.body);
+app.get('/currentUser', (req, res) => {
+  console.log(req.session);
 
+  res.json(req.session.user || {});
+});
+
+app.post('/logout', async (req, res) => {
+  console.log('logout', req.body);
+  delete req.session.user;
   res.json({
-    asd: 'asd'
+    success: true
   });
 });
 
+app.post('/login', async (req, res) => {
+  console.log('login attempt', req.body);
+
+  const user = await User.findOne({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  if (user) {
+    req.session.user = {
+      _id: user._id,
+      username: user.username
+    };
+    res.json({
+      success: true,
+      user: req.session.user
+    });
+  } else {
+    res.json({
+      success: false,
+      message: 'incorrect login details'
+    });
+  }
+});
+
 app.post('/register', async (req, res) => {
-  console.log('register', req.body);
+  console.log('register attempt', req.body);
 
   if (await User.exists({ username: req.body.username })) {
     res.json({
       success: false,
-      reason: 'username already taken'
+      message: 'username already taken'
     });
   } else {
     const user = new User({
